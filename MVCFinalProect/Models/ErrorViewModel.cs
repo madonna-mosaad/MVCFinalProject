115ce6406842cfd777ffd186724abd2bda1ea3df
madonna-mosaad <madonnamosaad86@gmail.com>
2024-09-27 16:06:20 +0300

    Add project files.

---
+++ MVCFinalProect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCFinalProect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
